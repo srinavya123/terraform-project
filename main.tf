1e5d206f383bab623b75ce1a7568c01f4646b006
srinavya123 <140142776+srinavya123@users.noreply.github.com>
2023-09-13 10:59:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 #this file consists of code for instances and sg
 provider "aws" {
-region = "ap-southeast-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0b825ad86ddcfb907"
+  ami             = "ami-0f844a9675b22ea32"
   instance_type   = "t2.micro"
-  key_name        = "singaporekey"
+  key_name        = "Ansible"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "us-east-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+echo "hai all this is my app created by terraform infrastructurte server-1" > /var/www/html/index.html
 EOF
   tags = {
     Name = "web-server-1"
@@ -23,18 +23,18 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0b825ad86ddcfb907"
+  ami             = "ami-0f844a9675b22ea32"
   instance_type   = "t2.micro"
-  key_name        = "singaporekey"
+  key_name        = "Ansible"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "us-east-1b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+echo "hai all this is my website created by terraform infrastructurte  server-2" > /var/www/html/index.html
 EOF
   tags = {
     Name = "web-server-2"
@@ -42,28 +42,50 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-0b825ad86ddcfb907"
+  ami             = "ami-0f844a9675b22ea32"
   instance_type   = "t2.micro"
-  key_name        = "singaporekey"
+  key_name        = "Ansible"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-0b825ad86ddcfb907"
+  ami             = "ami-0f844a9675b22ea32"
   instance_type   = "t2.micro"
-  key_name        = "singaporekey"
+  key_name        = "Ansible"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "app-server-2"
   }
 }
 
-resource "aws_security_group" "five" {
+resource "aws_instance" "five" {
+  ami             = "ami-0f844a9675b22ea32"
+  instance_type   = "t2.micro"
+  key_name        = "Ansible"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "db-server-1"
+  }
+}
+
+resource "aws_instance" "six" {
+  ami             = "ami-0f844a9675b22ea32"
+  instance_type   = "t2.micro"
+  key_name        = "Ansible"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "db-server-2"
+  }
+}
+
+resource "aws_security_group" "seven" {
   name = "elb-sg"
   ingress {
     from_port   = 22
@@ -87,11 +109,11 @@ resource "aws_security_group" "five" {
   }
 }
 
-resource "aws_s3_bucket" "six" {
-  bucket = "rahamterraserverbucketoo99"
+resource "aws_s3_bucket" "eight" {
+  bucket = "terraserverbucketoo99"
 }
 
-resource "aws_iam_user" "seven" {
+resource "aws_iam_user" "nine" {
 for_each = var.user_names
 name = each.value
 }
@@ -102,8 +124,8 @@ type = set(string)
 default = ["user1", "user2", "user3", "user4"]
 }
 
-resource "aws_ebs_volume" "eight" {
- availability_zone = "ap-southeast-1a"
+resource "aws_ebs_volume" "ten" {
+ availability_zone = "us-east-1a"
   size = 40
   tags = {
     Name = "ebs-001"
